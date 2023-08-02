2675b2db7599ce690be4e509b1f0a7d6db6afe51
yogeshvakhre05 <=yogeshvakhre05@gmail.com>
2023-08-02 15:34:41 +0530

    Added feature prodcut section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ function App() {
       <header>
         <a href="/">Amazona</a>
       </header>
-      <main></main>
+      <main>
+        <h1>Features Products</h1>
+        <div className="products">
+          <div className="product" key="product.slug">
+            <a href="/product/product.slug">
+              <img src="images/p1.jpg" alt="product" />
+            </a>
+            <div className="product-info">
+              <p>product.name</p>
+              <p>
+                <strong>product.price</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
