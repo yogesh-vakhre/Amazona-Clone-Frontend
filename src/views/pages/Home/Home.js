669bd2a6c98b8ccb7fb53e000d90c2f47e60c4a6
nihalkolidev <nihal.koli.dev@gmail.com>
2023-08-06 12:54:37 +0530

    Display products in feature

--- src/views/pages/Home/Home.js
+++ src/views/pages/Home/Home.js
@@ -1,24 +1,28 @@
 import React from "react";
-//import PropTypes from 'prop-types'
+import { loadProductsRequest } from "../../../store/actions/productActions";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import ProductItem from "../../../components/ProductItem/ProductItem";
 
 const Home = () => {
+  const {
+    product: { total, products, error, loader, success },
+  } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadProductsRequest());
+  }, []);
+
+  console.log("productsData", { total, products, error, loader, success });
   return (
     <>
       <main>
         <h1>Features Products</h1>
         <div className="products">
-          <div className="product" key="product.slug">
-            <a href="/product/product.slug">
-              <img src="images/p1.jpg" alt="product" />
-            </a>
-            <div className="product-info">
-              <p>product.name</p>
-              <p>
-                <strong>product.price</strong>
-              </p>
-              <button>Add to cart</button>
-            </div>
-          </div>
+          {products.map((product) => (
+            <ProductItem product={product} key={product.slug} />
+          ))}
         </div>
       </main>
     </>
