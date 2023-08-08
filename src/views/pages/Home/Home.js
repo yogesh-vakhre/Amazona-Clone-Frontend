07dd7f653e8701744647c56290b43f019cab4f11
nihalkolidev <nihal.koli.dev@gmail.com>
2023-08-08 17:40:21 +0530

    Added ProductItem and Rating components

--- src/views/pages/Home/Home.js
+++ src/views/pages/Home/Home.js
@@ -3,7 +3,8 @@ import { loadProductsRequest } from "../../../store/actions/productActions";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import ProductItem from "../../../components/ProductItem/ProductItem";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   const {
@@ -20,11 +21,19 @@ const Home = () => {
     <>
       <main>
         <Container>
+          <Helmet>
+            <title>Amazona</title>
+          </Helmet>
           <h1>Features Products</h1>
+
           <div className="products">
-            {products.map((product) => (
-              <ProductItem product={product} key={product.slug} />
-            ))}
+            <Row>
+              {products.map((product) => (
+                <Col sm={6} md={4} lg={3} className="mb-3">
+                  <ProductItem product={product} key={product.slug} />
+                </Col>
+              ))}
+            </Row>
           </div>
         </Container>
       </main>
