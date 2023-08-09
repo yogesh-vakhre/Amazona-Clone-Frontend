93da8257e7d226823efd1bd4dcfb14f9804fabb7
yogeshvakhre05 <=yogeshvakhre05@gmail.com>
2023-08-09 10:00:10 +0530

    Added Get product by Slug feature in store

--- src/store/actions/productActions.js
+++ src/store/actions/productActions.js
@@ -2,6 +2,9 @@ import {
   LOAD_PRODUCTS_FAILURE,
   LOAD_PRODUCTS_REQUEST,
   LOAD_PRODUCTS_SUCCESS,
+  LOAD_PRODUCT_BY_SLUG_FAILURE,
+  LOAD_PRODUCT_BY_SLUG_REQUEST,
+  LOAD_PRODUCT_BY_SLUG_SUCCESS,
 } from "../action-types/productActionTypes";
 
 export const loadProductsRequest = () => ({
@@ -15,5 +18,20 @@ export const loadProductsSucess = (data) => ({
 
 export const loadProductsFail = (error) => ({
   type: LOAD_PRODUCTS_FAILURE,
+  payload: error,
+});
+
+export const loadProductBySlugRequest = (slug) => ({
+  type: LOAD_PRODUCT_BY_SLUG_REQUEST,
+  payload: slug,
+});
+
+export const loadProductBySlugSucess = (data) => ({
+  type: LOAD_PRODUCT_BY_SLUG_SUCCESS,
+  payload: data,
+});
+
+export const loadProductBySlugFail = (error) => ({
+  type: LOAD_PRODUCT_BY_SLUG_FAILURE,
   payload: error,
 });
--- src/store/sagas/productSaga.js
+++ src/store/sagas/productSaga.js
@@ -1,9 +1,14 @@
 import { takeLatest, put, all, fork, call } from "redux-saga/effects";
 import {
+  loadProductBySlugFail,
+  loadProductBySlugSucess,
   loadProductsFail,
   loadProductsSucess,
 } from "../actions/productActions";
-import { LOAD_PRODUCTS_REQUEST } from "../action-types/productActionTypes";
+import {
+  LOAD_PRODUCTS_REQUEST,
+  LOAD_PRODUCT_BY_SLUG_REQUEST,
+} from "../action-types/productActionTypes";
 import ProductService from "../services/product.service";
 
 function* onLoadProductsRequestAsync() {
@@ -20,11 +25,33 @@ function* onLoadProductsRequestAsync() {
   }
 }
 
+function* onLoadProductBySlugRequestAsync({ payload }) {
+  try {
+    const response = yield call(ProductService.getBySlug, payload);
+    console.log("Call_Saga_Get_Product_BY_Slug", payload);
+    if (response.status === "success") {
+      yield put(loadProductBySlugSucess(response));
+    } else {
+      yield put(
+        loadProductBySlugFail("Something Went Wrong, Please Try Again!")
+      );
+    }
+  } catch (error) {
+    yield put(loadProductsFail(error.response));
+  }
+}
+
 export function* onLoadProducts() {
   yield takeLatest(LOAD_PRODUCTS_REQUEST, onLoadProductsRequestAsync);
 }
 
-const productSagas = [fork(onLoadProducts)];
+export function* onLoadProductBySlug() {
+  yield takeLatest(
+    LOAD_PRODUCT_BY_SLUG_REQUEST,
+    onLoadProductBySlugRequestAsync
+  );
+}
+const productSagas = [fork(onLoadProducts), fork(onLoadProductBySlug)];
 
 export default function* productSaga() {
   yield all([...productSagas]);
