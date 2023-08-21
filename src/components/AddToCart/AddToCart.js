5330d8c2f316d88164d2c9ab21260e38af17af58
yogeshvakhre05 <=yogeshvakhre05@gmail.com>
2023-08-21 16:42:54 +0530

    Added navigate cart when add to cart

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCartStart } from "../../store/actions/cartActions";
+import { useNavigate } from "react-router-dom";
 
 const AddToCart = (props) => {
   const { slug } = props;
   const {
     cart: { cart },
   } = useSelector((state) => state);
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   // Add to cart
@@ -17,6 +18,7 @@ const AddToCart = (props) => {
 
     // console.log("addToCartHandler", product);
     dispatch(addToCartStart({ slug, cart }));
+    navigate("/cart");
     //dispatch(loadProductBySlugRequest(slug));
   };
 
