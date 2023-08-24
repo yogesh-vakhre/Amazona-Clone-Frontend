b4b08655b929f866e9f203a1c2f5753c67541c28
yogesh-vakhre <yogeshvakhre05@gmail.com>
2023-08-24 15:12:10 +0530

    Added show order by id feature

--- src/store/reducers/orderReducer.js
+++ src/store/reducers/orderReducer.js
@@ -2,22 +2,26 @@ import {
   ADD_ORDER_ERROR,
   ADD_ORDER_START,
   ADD_ORDER_SUCCESS,
+  LOAD_ORDER_BY_ID_ERROR,
+  LOAD_ORDER_BY_ID_START,
+  LOAD_ORDER_BY_ID_SUCCESS,
 } from "../action-types/orderActionTypes";
 
 const initialState = {
-  total: 0,
   order: {},
   error: "",
-  loader: false,
   success: false,
+  loader: false,
 };
 
 const orderReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_ORDER_START:
+    case LOAD_ORDER_BY_ID_START:
       return { ...state, order: action.payload, loader: true };
 
     case ADD_ORDER_SUCCESS:
+    case LOAD_ORDER_BY_ID_SUCCESS:
       return {
         ...state,
         order: action.payload.order,
@@ -26,6 +30,7 @@ const orderReducer = (state = initialState, action) => {
       };
 
     case ADD_ORDER_ERROR:
+    case LOAD_ORDER_BY_ID_ERROR:
       return {
         ...state,
         error: action.payload,
---
+++ src/views/pages/Order/Order.js
@@ -0,0 +1,162 @@
+import React, { useEffect } from "react";
+import { Card, Col, Container, ListGroup, Row } from "react-bootstrap";
+import { Helmet } from "react-helmet-async";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import Preloader from "../../../components/Preloader/Preloader";
+import MessageBox from "../../../components/MessageBox/MessageBox";
+import { loadOrderByIdStart } from "../../../store/actions/orderActions";
+
+const Order = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const params = useParams();
+  const { orderId } = params;
+  const { isSignedIn = false } = useSelector((state) => state.auth);
+  const { cart } = useSelector((state) => state.cart);
+  const { loader, order } = useSelector((state) => state.order);
+
+  const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100; // 123.2345 => 123.23
+  cart.itemsPrice = round2(
+    cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
+  );
+  cart.shippingPrice = cart.itemsPrice > 100 ? round2(0) : round2(10);
+  cart.taxPrice = round2(0.15 * cart.itemsPrice);
+  cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+
+  useEffect(() => {
+    // Check user login
+    if (!isSignedIn) {
+      navigate("/signin?redirect=/shipping");
+    }
+    dispatch(loadOrderByIdStart(orderId));
+  }, [navigate, cart, isSignedIn]);
+
+  // Show lodder
+  if (loader) {
+    return <Preloader />;
+  }
+  return (
+    <>
+      <main>
+        <Container className="mt-3">
+          <Container>
+            <Helmet>
+              <title> Order {orderId}</title>
+            </Helmet>
+
+            <h1 className="my-3"> Order {orderId}</h1>
+            <Row>
+              <Col md={8}>
+                <Card className="mb-3">
+                  <Card.Body>
+                    <Card.Title>Shipping</Card.Title>
+                    <Card.Text>
+                      <strong>Name:</strong> {cart.shippingAddress.fullName}{" "}
+                      <br />
+                      <strong>Address: </strong> {cart.shippingAddress.address},
+                      {cart.shippingAddress.city},{" "}
+                      {cart.shippingAddress.postalCode},
+                      {cart.shippingAddress.country}
+                    </Card.Text>
+                    {order.isDelivered ? (
+                      <MessageBox variant="success">
+                        Delivered at {order.deliveredAt}
+                      </MessageBox>
+                    ) : (
+                      <MessageBox variant="danger">Not Delivered</MessageBox>
+                    )}
+                  </Card.Body>
+                </Card>
+
+                <Card className="mb-3">
+                  <Card.Body>
+                    <Card.Title>Payment</Card.Title>
+                    <Card.Text>
+                      <strong>Method:</strong> {cart.paymentMethod}
+                    </Card.Text>
+                    {order.isPaid ? (
+                      <MessageBox variant="success">
+                        Paid at {order.paidAt}
+                      </MessageBox>
+                    ) : (
+                      <MessageBox variant="danger">Not Paid</MessageBox>
+                    )}
+                  </Card.Body>
+                </Card>
+
+                <Card className="mb-3">
+                  <Card.Body>
+                    <Card.Title>Items</Card.Title>
+                    <ListGroup variant="flush">
+                      {cart.cartItems.map((item) => (
+                        <ListGroup.Item key={item._id}>
+                          <Row className="align-items-center">
+                            <Col md={6}>
+                              <img
+                                width={70}
+                                src={item.image}
+                                alt={item.name}
+                                className="img-fluid rounded img-thumbnail"
+                              ></img>{" "}
+                              <Link to={`/product/${item.slug}`}>
+                                {item.name}
+                              </Link>
+                            </Col>
+                            <Col md={3}>
+                              <span>{item.quantity}</span>
+                            </Col>
+                            <Col md={3}>${item.price}</Col>
+                          </Row>
+                        </ListGroup.Item>
+                      ))}
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col md={4}>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>Order Summary</Card.Title>
+                    <ListGroup variant="flush">
+                      <ListGroup.Item>
+                        <Row>
+                          <Col>Items</Col>
+                          <Col>${cart?.itemsPrice?.toFixed(2)}</Col>
+                        </Row>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <Row>
+                          <Col>Shipping</Col>
+                          <Col>${cart?.shippingPrice?.toFixed(2)}</Col>
+                        </Row>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <Row>
+                          <Col>Tax</Col>
+                          <Col>${cart?.taxPrice?.toFixed(2)}</Col>
+                        </Row>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <Row>
+                          <Col>
+                            <strong> Order Total</strong>
+                          </Col>
+                          <Col>
+                            <strong>${cart?.totalPrice?.toFixed(2)}</strong>
+                          </Col>
+                        </Row>
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      </main>
+    </>
+  );
+};
+
+export default Order;
