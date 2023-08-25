07e4da518852cb07bb097edea6dfde788ab0c00f
yogesh-vakhre <yogeshvakhre05@gmail.com>
2023-08-25 19:07:10 +0530

    Added forget password feature

--- src/store/services/auth.service.js
+++ src/store/services/auth.service.js
@@ -43,9 +43,24 @@ const updateProfile = async (data) => {
   }
 };
 
+const forgetPassword = async (data) => {
+  try {
+    const response = await authInstance
+      .post("/forgot-password", data)
+      .catch(function (error) {
+        toast.error(error.response.data.message);
+        return console.error(error.response.data);
+      });
+    return response.data;
+  } catch (error) {
+    return console.error(error);
+  }
+};
+
 const AuthService = {
   signIn,
   signUp,
   updateProfile,
+  forgetPassword,
 };
 export default AuthService;
